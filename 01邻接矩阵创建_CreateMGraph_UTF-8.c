0a8d049f8f86d81eaf39b40a3ebee4d6d48f98dc
kryiea <36979568+kryiea@users.noreply.github.com>
2022-12-06 17:39:18 +0800

    Add files via upload

---
+++ 01邻接矩阵创建_CreateMGraph_UTF-8.c
@@ -0,0 +1,50 @@
+#include "stdio.h"    
+#include "stdlib.h"   
+#include "math.h"  
+#include "time.h"
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+#define MAXVEX 100 /* ��󶥵�����Ӧ���û����� */
+#define GRAPH_INFINITY 65535 /* ��65535�������� */
+
+typedef int Status;	/* Status�Ǻ���������,��ֵ�Ǻ������״̬���룬��OK�� */
+typedef char VertexType; /* ��������Ӧ���û�����  */
+typedef int EdgeType; /* ���ϵ�Ȩֵ����Ӧ���û����� */
+typedef struct
+{
+	VertexType vexs[MAXVEX]; /* ����� */
+	EdgeType arc[MAXVEX][MAXVEX];/* �ڽӾ��󣬿ɿ����߱� */
+	int numNodes, numEdges; /* ͼ�е�ǰ�Ķ������ͱ���  */
+}MGraph;
+
+/* ����������ͼ���ڽӾ����ʾ */
+void CreateMGraph(MGraph *G)
+{
+	int i,j,k,w;
+	printf("���붥�����ͱ���:\n");
+	scanf("%d,%d",&G->numNodes,&G->numEdges); /* ���붥�����ͱ��� */
+	for(i = 0;i <G->numNodes;i++) /* ���붥����Ϣ,��������� */
+		scanf(&G->vexs[i]);
+	for(i = 0;i <G->numNodes;i++)
+		for(j = 0;j <G->numNodes;j++)
+			G->arc[i][j]=GRAPH_INFINITY;	/* �ڽӾ����ʼ�� */
+	for(k = 0;k <G->numEdges;k++) /* ����numEdges���ߣ������ڽӾ��� */
+	{
+		printf("�����(vi,vj)�ϵ��±�i���±�j��Ȩw:\n");
+		scanf("%d,%d,%d",&i,&j,&w); /* �����(vi,vj)�ϵ�Ȩw */
+		G->arc[i][j]=w; 
+		G->arc[j][i]= G->arc[i][j]; /* ��Ϊ������ͼ������Գ� */
+
+	}
+}
+
+int main(void)
+{    
+	MGraph G;    
+	CreateMGraph(&G);
+	
+	return 0;
+}
